4507f54083ed37490bb0608d2c4c9a44b003f261
HonWiCong <102761891@students.swinburne.edu.my>
2024-05-20 12:10:37 +0800

    change password

--- src/lib/rds.ts
+++ src/lib/rds.ts
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 export const database = await mysql.createConnection({
 	host: "database.ckozhfjjzal0.us-east-1.rds.amazonaws.com",
 	user: "admin",
-	password: "password",
+	password: "aRHnjDuknZhPZc4",
 	database: "parking",
 	port: 3306,
 });
